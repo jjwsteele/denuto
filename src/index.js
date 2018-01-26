43f35862fe67e4626879da22ecafbc6568b33423
Jordan Steele <jjwsteele@gmail.com>
2018-01-26 17:26:38 +1100

    Remove pureContract from index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import contract from './contract'
-import pureContract from './pureContract'
 import invariant from './invariant'
 import require from './require'
 import ensure from './ensure'
 
 export {
   contract,
-  pureContract,
   invariant,
   require,
   ensure,
